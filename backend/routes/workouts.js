5cb0d53cc38892c201c218499867821e656d7fdf
kure690 <bobtani2234@gmail.com>
2024-07-09 17:09:41 +0800

    initial commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
+
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single workout'})
+})
+
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+    try{
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    }
+    catch (error){
+        res.status(400).json({error: error.message})
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a workout'})
+})
+
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a workout'})
+})
+
+module.exports = router
